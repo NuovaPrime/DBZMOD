52175cd002218e0815b625dbb7c5479841bfc79f
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2018-07-11 14:41:04 -0400

    Destructo

--- Projectiles/DestructoDiskProjectile.cs
+++ Projectiles/DestructoDiskProjectile.cs
@@ -20,7 +20,7 @@ namespace DBZMOD.Projectiles
         {
             projectile.width = 62;
             projectile.height = 62;
-			projectile.aiStyle = 52;
+			projectile.aiStyle = 1;
 			projectile.light = 1f;
             projectile.friendly = true;
             projectile.ignoreWater = true;
