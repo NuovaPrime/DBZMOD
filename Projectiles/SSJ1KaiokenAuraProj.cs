bd0be4bf034bd7943975885183f379a4052ea9e5
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2018-07-12 16:52:03 -0400

    Revert "Revert "SSJKKAURA""
    This reverts commit 7e7c9e9fbb47f8803693787c38c7e08d2b55f9d1.

---
+++ Projectiles/SSJ1KaiokenAuraProj.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Projectiles
+{
+    public class SSJ1KaiokenAuraProj : ModProjectile
+    {
+        public float KaioAuraTimer;
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[projectile.type] = 4;
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 95;
+            projectile.height = 89;
+            projectile.aiStyle = 0;
+            projectile.alpha = 70;
+            projectile.timeLeft = 10;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.penetrate = -1;
+            projectile.damage = 0;
+            KaioAuraTimer = 240;
+        }
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+            projectile.position.X = player.Center.X;
+            projectile.position.Y = player.Center.Y;
+            projectile.Center = player.Center + new Vector2(0, -25);
+            projectile.netUpdate = true;
+            if (!player.HasBuff(mod.BuffType("SSJ1KaiokenAuraProj")))
+            {
+                projectile.Kill();
+            }
+            if (projectile.timeLeft < 2)
+            {
+                projectile.timeLeft = 10;
+            }
+            projectile.frameCounter++;
+            if (projectile.frameCounter > 5)
+            {
+                projectile.frame++;
+                projectile.frameCounter = 0;
+            }
+            if (projectile.frame >= 4)
+            {
+                projectile.frame = 0;
+            }
+            if (KaioAuraTimer > 0)
+            {
+                projectile.scale = 1.5f + 2f * (KaioAuraTimer / 240f);
+                KaioAuraTimer--;
+            }
+            else
+            {
+                projectile.scale = 1.5f;
+            }
+        }
+    }
+}
