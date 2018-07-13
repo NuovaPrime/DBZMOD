592e96268ed091e5801e4d9326fc1955ea4911e0
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2018-07-13 15:01:00 -0400

    NEW: Scatter Shot

---
+++ Items/Weapons/Scattershot.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Items.Weapons
+{
+	public class Scattershot : KiItem
+	{
+		public override void SetDefaults()
+		{
+			// Alter any of these values as you see fit, but you should probably keep useStyle on 1, as well as the noUseGraphic and noMelee bools
+			item.shoot = mod.ProjectileType("ScattershotBlast");
+			item.shootSpeed = 17f;
+			item.damage = 64;
+			item.knockBack = 2f;
+			item.useStyle = 5;
+			item.UseSound = SoundID.Item12;
+			item.useAnimation = 90;
+			item.useTime = 90;
+			item.width = 40;
+			item.noUseGraphic = true;
+			item.height = 40;
+			item.autoReuse = false;
+			item.value = Item.sellPrice(0, 0, 5, 0);
+			item.rare = 4;
+            item.channel = true;
+            KiDrain = 150;
+	    }
+	    public override void SetStaticDefaults()
+		{
+		Tooltip.SetDefault("-Tier 4-");
+		DisplayName.SetDefault("Scatter Shot");
+		}
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			{
+			int numberProjectiles = 8 + Main.rand.Next(2); // 4 or 5 shots
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(25)); // 30 degree spread.
+				// If you want to randomize the speed to stagger the projectiles
+			 float scale = 1f - (Main.rand.NextFloat() * .4f);
+			 perturbedSpeed = perturbedSpeed * scale; 
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+			}
+			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 12f;
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+	        recipe.AddIngredient(null, "HonorKiCrystal", 30);
+            recipe.AddIngredient(null, "CalmKiCrystal", 30);
+            recipe.AddIngredient(null, "Masenko");
+			recipe.AddIngredient(null, "HellzoneGrenade");
+            recipe.AddTile(null, "KiManipulator");
+            recipe.SetResult(this);
+	        recipe.AddRecipe();
+		}
+	}
+}
