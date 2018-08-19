5d9e95974c749b6e0afe6c909c9cc937b056186b
JavidPack <javidpack@gmail.com>
2018-08-18 23:17:56 -0600

    Fix Ki UI, Gradient.

--- UI/ResourceBar.cs
+++ UI/ResourceBar.cs
@@ -1,4 +1,4 @@
-﻿﻿using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
@@ -8,101 +8,110 @@ using Terraria.UI;
 
 namespace DBZMOD.UI
 {
-    internal enum ResourceBarMode
-    {
-        KI
-    }
-    class ResourceBar : UIElement
-    {
-        private ResourceBarMode stat;
-        private float width;
-        private float height;
+	internal enum ResourceBarMode
+	{
+		KI
+	}
+	class ResourceBar : UIElement
+	{
+		private ResourceBarMode stat;
+		private float width;
+		private float height;
 
-        public ResourceBar(ResourceBarMode stat, int height, int width)
-        {
-            this.stat = stat;
-            this.width = width;
-            this.height = height;
-        }
-        private UIFlatPanel currentBar;
-        private UIText text;
+		public ResourceBar(ResourceBarMode stat, int height, int width)
+		{
+			this.stat = stat;
+			this.width = width;
+			this.height = height;
+		}
+		private UIText text;
 
-        public override void OnInitialize()
-        {
-            Height.Set(height, 0f); //Set Height of element
-            Width.Set(width, 0f);   //Set Width of element
+		Rectangle barDestination;
+		private Color gradientA;
+		private Color gradientB;
 
-            UIFlatPanel barBackground = new UIFlatPanel(); //Create gray background
-            barBackground.Left.Set(0f, 0f);
-            barBackground.Top.Set(0f, 0f);
-            barBackground.backgroundColor = new Color(0f, 0f, 0f, 0);
-            barBackground.Width.Set(width, 0f);
-            barBackground.Height.Set(height, 0f);
+		public override void OnInitialize()
+		{
+			Height.Set(height, 0f); //Set Height of element
+			Width.Set(width, 0f);   //Set Width of element
 
-            currentBar = new UIFlatPanel(); //Create current value panel 
-            currentBar.SetPadding(0);
-            currentBar.Left.Set(0f, 0f);
-            currentBar.Top.Set(0f, 0f);
-            currentBar.Width.Set(width, 0f);
-            currentBar.Height.Set(height, 0f);
+			//assign color to panel depending on stat
+			switch (stat)
+			{
+				case ResourceBarMode.KI:
+					gradientA = new Color(0, 255, 255); //blue
+					gradientB = Color.Red;
+					break;
+				default:
+					break;
+			}
 
+			text = new UIText("0/0"); //text to show current hp or mana
+			text.Width.Set(width, 0f);
+			text.Height.Set(height, 0f);
+			text.Top.Set(height / 2 + 10, 0f); //center the UIText
+			text.Left.Set(width - 45, 0f);
 
-            //assign color to panel depending on stat
-            switch (stat)
-            {
-                case ResourceBarMode.KI:
-                    currentBar.backgroundColor = new Color(0, 255, 255); //blue
-                    break;
-                default:
-                    break;
-            }
+			var BarTexture = GFX.KiBar;
+			UIImage ki = new UIImage(BarTexture);
+			ki.Top.Set(-8, 0f);
+			ki.Width.Set(80, 0f);
+			ki.Height.Set(18, 0f);
+			Append(ki);
 
-            text = new UIText("0/0"); //text to show current hp or mana
-            text.Width.Set(width, 0f);
-            text.Height.Set(height, 0f);
-            text.Top.Set(height / 2 + 10, 0f); //center the UIText
-            text.Left.Set(width - 100, 0f);
-            
-                        
-            barBackground.Append(currentBar);
-            barBackground.Append(text);
-            base.Append(barBackground);
-        }
-        
-            
-        public override void Draw(SpriteBatch spriteBatch)
-        {
-            MyPlayer player = Main.LocalPlayer.GetModPlayer<MyPlayer>();
-            float quotient = 1f;
-            //Calculate quotient
-            switch (stat)
-            {
-                case ResourceBarMode.KI:
-                    quotient = (float)player.KiCurrent / (float)player.KiMax;
-                    break;
+			Append(text);
 
-                default:
-                    break;
-            }
-            currentBar.Width.Set(quotient * width, 0f);
-            Recalculate(); // recalculate the position and size
+			barDestination = new Rectangle(20, 0, (int)width, (int)height);
+		}
 
-            base.Draw(spriteBatch);
-        }
-        public override void Update(GameTime gameTime)
-        {
-            MyPlayer player = Main.LocalPlayer.GetModPlayer<MyPlayer>(); //Get Player
-            switch (stat)
-            {
-                case ResourceBarMode.KI:
-                    text.SetText("Ki:" + player.KiCurrent + " / " + player.KiMax); //Set Life
-                    break;
+		protected override void DrawSelf(SpriteBatch spriteBatch)
+		{
+			base.DrawSelf(spriteBatch); // draws nothing since this inherits from UIElement
 
-                default:
-                    break;
-            }
-            base.Update(gameTime);
-        }
-        
-    }
+			MyPlayer player = Main.LocalPlayer.GetModPlayer<MyPlayer>();
+			float quotient = 1f;
+			//Calculate quotient
+			switch (stat)
+			{
+				case ResourceBarMode.KI:
+					quotient = (float)player.KiCurrent / (float)player.KiMax;
+					quotient = Utils.Clamp(quotient, 0, 1);
+					break;
+
+				default:
+					break;
+			}
+
+			Rectangle hitbox = GetInnerDimensions().ToRectangle();
+			hitbox.X += barDestination.X;
+			hitbox.Y += barDestination.Y;
+			hitbox.Width = barDestination.Width;
+			hitbox.Height = barDestination.Height;
+			int left = hitbox.Left;
+			int right = hitbox.Right;
+			int steps = (int)((right - left) * quotient);
+			for (int i = 0; i < steps; i += 1)
+			{
+				//float percent = (float)i / steps; // Alternate Gradient Approach
+				float percent = (float)i / (right - left);
+				spriteBatch.Draw(Main.magicPixel, new Rectangle(left + i, hitbox.Y, 1, hitbox.Height), Color.Lerp(gradientA, gradientB, percent));
+			}
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			MyPlayer player = Main.LocalPlayer.GetModPlayer<MyPlayer>(); //Get Player
+			switch (stat)
+			{
+				case ResourceBarMode.KI:
+					text.SetText("Ki:" + player.KiCurrent + " / " + player.KiMax); //Set Life
+					break;
+
+				default:
+					break;
+			}
+			base.Update(gameTime);
+		}
+
+	}
 }
