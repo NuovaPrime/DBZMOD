72698577e3ee3c43b2c784a5e37eca32c2071582
NuovaPrime <loganman51@hotmail.com>
2018-09-01 14:01:27 -0600

    Sprite upload + lssj menu icon

--- GFX.cs
+++ GFX.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Audio;
 
 namespace DBZMOD
 {
-    public static class GFX
+    public class GFX
     {
 
         private const string GUI_DIRECTORY = "GFX/";
@@ -16,28 +16,32 @@ namespace DBZMOD
         private const string KIBAR = UI_DIRECTORY + "KiBar";
         private const string SSJ1BUTTON = UI_DIRECTORY + "SSJ1ButtonImage";
         private const string SSJ2BUTTON = UI_DIRECTORY + "SSJ2ButtonImage";
+        private const string LSSJBUTTON = UI_DIRECTORY + "LSSJButtonImage";
         private const string SSJ3BUTTON = UI_DIRECTORY + "SSJ3ButtonImage";
         private const string BG = UI_DIRECTORY + "Bg";
         private const string HAIR_DIRECTORY = "HAIR/";
-        //private const string SSJ1HAIR = HAIR_DIRECTORY + "SSJ1Hair";
-        //private const string SSJ2HAIR = HAIR_DIRECTORY + "SSJ2Hair";
 
         public static Texture2D KiBar;
         public static Texture2D Bg;
         public static Texture2D SSJ1ButtonImage;
         public static Texture2D SSJ2ButtonImage;
         public static Texture2D SSJ3ButtonImage;
-        // public static Texture2D SSJ1Hair;
-        //public static Texture2D SSJ2Hair;
+        private static Player player;
+
         public static void LoadGFX(Mod mod)
         {
             KiBar = mod.GetTexture(KIBAR);
             Bg = mod.GetTexture(BG);
             SSJ1ButtonImage = mod.GetTexture(SSJ1BUTTON);
-            SSJ2ButtonImage = mod.GetTexture(SSJ2BUTTON);
+            if(MyPlayer.ModPlayer(player).hasLegendary)
+            {
+                SSJ2ButtonImage = mod.GetTexture(LSSJBUTTON);
+            }
+            else
+            {
+                SSJ2ButtonImage = mod.GetTexture(SSJ2BUTTON);
+            }
             SSJ3ButtonImage = mod.GetTexture(SSJ3BUTTON);
-            //SSJ1Hair = mod.GetTexture(SSJ1HAIR);
-            //SSJ2Hair = mod.GetTexture(SSJ2HAIR);
 
         }
 
@@ -48,9 +52,6 @@ namespace DBZMOD
             SSJ1ButtonImage = null;
             SSJ2ButtonImage = null;
             SSJ3ButtonImage = null;
-            //SSJ1Hair = null;
-            //SSJ2Hair = null;
-            //SSJHairDraw.Hair = null;
         }
     }
 }
