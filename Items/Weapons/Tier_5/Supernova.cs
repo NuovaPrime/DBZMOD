26fa0fda472e6214c867ec15929925930e151a71
CanadianMRE <jaymen100@gmail.com>
2018-09-29 00:04:38 -0600

    Added Supernova

--- Items/Weapons/Tier_5/Supernova.cs
+++ Items/Weapons/Tier_5/Supernova.cs
@@ -7,21 +7,21 @@ using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace DBZMOD.Items.Weapons.Tier_4
+namespace DBZMOD.Items.Weapons.Tier_5
 {
-	public class SpiritBomb : KiItem
+	public class Supernova : KiItem
 	{
 		public override void SetDefaults()
 		{
 			// Alter any of these values as you see fit, but you should probably keep useStyle on 1, as well as the noUseGraphic and noMelee bools
-			item.shoot = mod.ProjectileType("SpiritBombBall");
-            item.shootSpeed = 6f;
-            item.damage = 80;
+			item.shoot = mod.ProjectileType("Supernova");
+            item.shootSpeed = 4f;
+            item.damage = 140;
             item.knockBack = 12f;
             item.useStyle = 1;
             item.UseSound = SoundID.Item1;
-            item.useAnimation = 300;
-            item.useTime = 300;
+            item.useAnimation = 250;
+            item.useTime = 250;
             item.width = 40;
             item.channel = true;
             item.noUseGraphic = true;
@@ -33,13 +33,13 @@ namespace DBZMOD.Items.Weapons.Tier_4
 
             }
             item.value = 0;
-            item.rare = 4;
-            KiDrain = 200;
+            item.rare = 7;
+            KiDrain = 300;
         }
 	    public override void SetStaticDefaults()
 		{
-		Tooltip.SetDefault("-Tier 4-");
-		DisplayName.SetDefault("Spirit Bomb");
+		Tooltip.SetDefault("-Tier 5-");
+		DisplayName.SetDefault("Supernova");
 		}
 
         //public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
@@ -55,15 +55,15 @@ namespace DBZMOD.Items.Weapons.Tier_4
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(null, "PridefulKiCrystal", 50);
-            recipe.AddIngredient(null, "AngerKiCrystal", 50);
+            recipe.AddIngredient(null, "SpiritBomb", 1);
+            recipe.AddIngredient(null, "PureKiCrystal", 50);
             recipe.AddTile(null, "ZTable");
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
         public override bool CanUseItem(Player player)
         {
-            if (player.ownedProjectileCounts[mod.ProjectileType("SpiritBombBall")] > 1)
+            if (player.ownedProjectileCounts[mod.ProjectileType("SupernovaBall")] > 1)
             {
                 return false;
             }
