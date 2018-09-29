410a46a8aafaff46f038344ff573b88e84528b43
NuovaPrime <loganman51@hotmail.com>
2018-09-28 22:47:38 -0600

    Update kamehamex10 again

---
+++ Items/Weapons/Tier_5/Kamehamehax10.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Items.Weapons.Tier_5
+{
+	public class Kamehamehax10 : KiItem
+	{
+		public override void SetDefaults()
+		{
+			item.shoot = mod.ProjectileType("Kamehamehax10Ball");
+			item.shootSpeed = 0f;
+			item.damage = 110;
+			item.knockBack = 2f;
+			item.useStyle = 5;
+			item.UseSound = SoundID.Item12;
+            item.channel = true;
+			item.useAnimation = 200;
+			item.useTime = 200;
+			item.width = 36;
+			item.noUseGraphic = true;
+			item.height = 36;
+			item.autoReuse = false;
+			item.value = 0;
+			item.rare = 7;
+            KiDrain = 120;
+	    }
+	    public override void SetStaticDefaults()
+		{
+		Tooltip.SetDefault("-Tier 5-" +
+		                   "\nMaximum Charges = 8");
+		DisplayName.SetDefault("Kamehameha x10");
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+	        recipe.AddIngredient(null, "PureKiCrystal", 40);
+			recipe.AddIngredient(null, "SuperKamehameha");
+            recipe.AddTile(null, "ZTable");
+            recipe.SetResult(this);
+	        recipe.AddRecipe();
+		}
+        public override bool CanUseItem(Player player)
+        {
+            if (player.ownedProjectileCounts[mod.ProjectileType("Kamehamehax10Ball")] > 1)
+            {
+                return false;
+            }
+            return base.CanUseItem(player);
+        }
+    }
+}
