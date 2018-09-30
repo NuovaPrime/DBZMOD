28c2e978a472a65d7d36da9fe2a8a803409db1a0
CanadianMRE <jaymen100@gmail.com>
2018-09-30 13:01:50 -0600

    Made Super Spirit Bomb

---
+++ Projectiles/SuperSpiritBombExplosion.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Projectiles
+{
+	public class SuperSpiritBombExplosion : KiProjectile
+	{
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.SwordBeam);
+            projectile.hostile = false;
+            projectile.friendly = true;
+			projectile.tileCollide = false;
+            projectile.width = 28;
+            projectile.height = 28;
+			projectile.aiStyle = 1;
+			projectile.light = 0.0f;
+			projectile.timeLeft = 10;
+			projectile.damage = 0;
+			aiType = 14;
+            projectile.ignoreWater = true;
+			projectile.penetrate = -1;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 4;
+            projectile.netUpdate = true;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+            projectile.hide = true;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("SuperSpiritBombExplosion");
+        }
+		
+		public override void Kill(int timeLeft)
+        {
+            if (!projectile.active)
+            {
+                return;
+            }
+
+            projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+            projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+            projectile.knockBack = 8f;
+            projectile.Damage();
+
+            projectile.active = false;
+			}
+		
+        public override void AI()
+        {
+            projectile.netUpdate = true;
+        }
+	}
+}
