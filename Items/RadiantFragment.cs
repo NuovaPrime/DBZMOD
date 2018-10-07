afe3c5496e6f72db0aa07d2590bebd31cacdddd6
TheRealEarth <33378647+TheRealEarth@users.noreply.github.com>
2018-10-07 13:42:15 -0400

    there
    aa

--- Items/RadiantFragment.cs
+++ Items/RadiantFragment.cs
@@ -15,7 +15,7 @@ namespace DBZMOD.Items
 			DisplayName.SetDefault("Radiant Fragment");
 			Tooltip.SetDefault("'The endurance of the cosmos crackles around this fragment'");
 		    ItemID.Sets.ItemNoGravity[item.type] = true;
-		    ItemID.Sets.AnimatesAsSoul[item.type] = true;
+	        Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(1, 1));
             ItemID.Sets.ItemIconPulse[item.type] = true;
 		}
 
