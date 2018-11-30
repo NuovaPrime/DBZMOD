5844a940403f38f807ea04c7e9d8625e9e213e92
MercuriusXeno <mercuriusxeno@gmail.com>
2018-11-30 11:38:45 -0600

    More zanzoken progress, working zoop

--- FistSystem.cs
+++ FistSystem.cs
@@ -211,45 +211,111 @@ namespace DBZMOD
         }
 
         // change this to change how far the player can teleport.
-        public const int ZANZOKEN_TRAVEL_DISTANCE = 50;
-        // returns the vertical/horizontal Vector offsets of a 45 degree angle that travels ZANZOKEN_TRAVEL_DISTANCE.
-        private int GetZanzokenDiagonalDistanceComponent()
+        public const float BASE_ZANZOKEN_TRAVEL_DISTANCE = 200f;
+
+        public float GetZanzokenDistance()
         {
-            var hypotenuse = ZANZOKEN_TRAVEL_DISTANCE;
-            var componentDistance = (int)Math.Ceiling(Math.Sqrt((hypotenuse * hypotenuse) / 2));
+            // TODO
+            // insert things that affect zanzoken distance here, like accessories or unlocked abilities.
+            return BASE_ZANZOKEN_TRAVEL_DISTANCE;
+        }
+
+        // returns the vertical/horizontal Vector offsets of a 45 degree angle that travels ZANZOKEN_TRAVEL_DISTANCE.
+        private float GetZanzokenDiagonalDistanceComponent()
+        {
+            var hypotenuse = GetZanzokenDistance();
+            var componentDistance = (float)Math.Sqrt((hypotenuse * hypotenuse) / 2);
             return componentDistance;
         }
 
         // returns a major rectangle responsible for the long range area in the teleport "cone" to scan for enemies.
-        private int GetZanzokenScanMajorSize()
+        private float GetZanzokenScanMajorSize()
         {
-            var rectangleBisectWidth = (int)Math.Floor(GetZanzokenDiagonalDistanceComponent() / 2f);
+            var rectangleBisectWidth = (float)GetZanzokenDiagonalDistanceComponent() / 2f;
             return rectangleBisectWidth;
         }
 
         // returns a minor rectangle responsible for the close area in the teleport "cone" to scan for enemies.
-        private int GetZanzokenScanMinorSize()
+        private float GetZanzokenScanMinorSize()
         {
-            var rectangleBisectWidth = (int)Math.Floor(GetZanzokenScanMajorSize() / 2f);
+            var rectangleBisectWidth = (float)GetZanzokenScanMajorSize() / 2f;
             return rectangleBisectWidth;
+        }
+
+        private bool CanZanzoken(Player player)
+        {
+            return !player.frozen && !player.stoned && !player.HasBuff(BuffID.Cursed);
         }
 
         public void PerformZanzoken(Player player, params Controls[] directions)
         {
+            // checks that would prevent you from using Zanzoken
+            if (!CanZanzoken(player))
+            {
+                return;
+            }
+
             // if the directions array contains more than one parameter, this is a diagonal zanzoken.
-            int offset = ZANZOKEN_TRAVEL_DISTANCE;
+            float offset = GetZanzokenDistance();
             if (directions.Length > 1)
             {
                 offset = GetZanzokenDiagonalDistanceComponent();
             }
 
-            // lazy switch to list so I can use linq.
+            // get the player's origin
+            Vector2 origin = new Vector2(player.position.X, player.position.Y);
+
+            // TODO  !!!
+            // spawn the zanzoken projectile/effect at the player's origin
+
+            // lazy switch to list so I can use Contains.
             List<Controls> directionList = new List<Controls>(directions);
 
-            int yOffset = (directionList.Contains(Controls.Up) ? -1 : (directionList.Contains(Controls.Down) ? 1 : 0)) * offset;
-            int xOffset = (directionList.Contains(Controls.Left) ? -1 : (directionList.Contains(Controls.Right) ? 1 : 0)) * offset;
+            // figure out where the player is going to try and end up.
+            float yOffset = (directionList.Contains(Controls.Up) ? -1 : (directionList.Contains(Controls.Down) ? 1 : 0)) * offset;
+            float xOffset = (directionList.Contains(Controls.Left) ? -1 : (directionList.Contains(Controls.Right) ? 1 : 0)) * offset;
 
-            // the easiest way to do this is with a projectile.
+            // this is a lazy hack, do a pixel by pixel "scan" until the player hits something, check if its an enemy and then place them in an uncollided position
+            float stepMaximum = Math.Max(Math.Abs(xOffset), Math.Abs(yOffset));
+            float xStep = xOffset / stepMaximum;
+            float yStep = yOffset / stepMaximum;
+            for (float f = 0f; f < stepMaximum; f += 1.0f)
+            {
+                float xPos = xStep * f;
+                float yPos = yStep * f;
+                Vector2 newPosition = origin + new Vector2(xPos, yPos);
+
+                // do a tile collision check. if this returns anything other than new position, we have collision.
+                bool isCollided = Collision.SolidCollision(newPosition, player.width, player.height);
+                
+                if (isCollided)
+                {
+                    // we collided with a thing.
+                    return;
+                }
+
+                player.position = newPosition;
+                
+                foreach(NPC npc in Main.npc)
+                {
+                    if (!npc.active || npc.friendly)
+                        continue;
+                    var npcRect = npc.getRect();
+                    var playerRect = player.getRect();
+                    if (npcRect.Intersects(playerRect))
+                    {
+                        // do npc-based teleport to a safe position
+                        PerformSafeZanzokenToNPC(player, npc);
+                        return;
+                    }
+                }                
+            }
+        }
+
+        public void PerformSafeZanzokenToNPC(Player player, NPC npc)
+        {
+            // TODO
+            // Actually do the safe zan.
         }
     }
 }
