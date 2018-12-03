1f7a382fff6091469bf884c8ba41811d41fd6942
MercuriusXeno <mercuriusxeno@gmail.com>
2018-12-03 12:22:41 -0600

    Oops. Debug outs in radiant glider wingUpdate

--- Items/Accessories/RadiantGlider.cs
+++ Items/Accessories/RadiantGlider.cs
@@ -27,7 +27,7 @@ namespace DBZMOD.Items.Accessories
 
         public override bool WingUpdate(Player player, bool inUse)
         {         
-            Main.NewText(string.Format("Wing: {0} Frame: {1} Counter: {2}", player.wings, player.wingFrame, player.wingFrameCounter));
+            // Main.NewText(string.Format("Wing: {0} Frame: {1} Counter: {2}", player.wings, player.wingFrame, player.wingFrameCounter));
             
             if (inUse)
             {
