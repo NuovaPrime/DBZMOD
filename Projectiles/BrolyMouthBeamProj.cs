a655bcdc4b7bda6b4039bb6057a3c9e0939268a9
MercuriusXeno <mercuriusxeno@gmail.com>
2018-12-15 14:49:28 -0600

    Initial branch commit. Baseline test objects, unabashedly stolen from blushie.

---
+++ Projectiles/BrolyMouthBeamProj.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Audio;
+using Util;
+
+namespace DBZMOD.Projectiles
+{
+	public class BrolyMouthBeamProj : BaseBeamProj
+	{
+        
+	}
+}
