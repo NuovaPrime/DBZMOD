563712682f91619f865a3deb2c3ce045bf86111f
NuovaPrime <loganman51@hotmail.com>
2018-12-21 15:11:20 -0700

    Dragon radars

--- Items/DragonBalls/DragonRadar1.cs
+++ Items/DragonBalls/DragonRadar1.cs
@@ -4,13 +4,13 @@ using Terraria.ModLoader;
 
 namespace DBZMOD.Items.DragonBalls
 {
-    public class DragonRadar : ModItem
+    public class DragonRadar1 : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Dragon Radar");
-            Tooltip.SetDefault("A high tech piece of equipment used to locate dragon balls." +
-                "\nHolding this will point you in the direction of the nearest dragon ball," +
+            DisplayName.SetDefault("Dragon Radar MK1");
+            Tooltip.SetDefault("A low tech piece of equipment used to locate dragon balls." +
+                "\nHolding this will point you in the direction of the nearest dragon ball with bad accuracy," +
                 "\nGetting too close to a dragon ball will overload the radar.");
         }
 
---
+++ Items/DragonBalls/DragonRadar2.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Items.DragonBalls
+{
+    public class DragonRadar2 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dragon Radar MK2");
+            Tooltip.SetDefault("A medium tech piece of equipment used to locate dragon balls." +
+                "\nHolding this will point you in the direction of the nearest dragon ball with reasonable accuracy," +
+                "\nGetting too close to a dragon ball will overload the radar.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.maxStack = 1;
+            item.value = 0;
+            item.rare = -12;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "DragonRadar1", 1);
+            recipe.AddIngredient(null, "AngerKiCrystal", 20);
+            recipe.AddIngredient(null, "RefinedMetal", 12);
+            recipe.AddTile(null, "ZTable");
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
