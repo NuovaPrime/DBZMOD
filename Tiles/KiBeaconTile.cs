bc4261126ceb4df886e168f87aa2b8cc18c60636
MercuriusXeno <mercuriusxeno@gmail.com>
2019-01-02 00:15:29 -0600

    Attempts at Ki Beacon Tile light and mostly failure. Ki Beacon Tile anims/glowmasks.

--- Tiles/KiBeaconTile.cs
+++ Tiles/KiBeaconTile.cs
@@ -1,4 +1,6 @@
-﻿﻿using Microsoft.Xna.Framework;
+﻿using DBZMOD.Util;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
@@ -12,7 +14,7 @@ namespace DBZMOD.Tiles
     {
         public override void SetDefaults()
         {
-            Main.tileSolidTop[Type] = false;
+            Main.tileSolidTop[Type] = true;
             Main.tileFrameImportant[Type] = true;
             Main.tileNoAttach[Type] = true;
             Main.tileTable[Type] = false;
@@ -22,15 +24,30 @@ namespace DBZMOD.Tiles
             TileObjectData.newTile.CoordinateWidth = 16;
             TileObjectData.newTile.Width = 3;
             TileObjectData.newTile.Height = 1;
-            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 };
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16 };
             TileObjectData.newTile.Origin = new Point16(1, 0);
-            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.CoordinatePadding = 2;            
             ModTranslation name = CreateMapEntryName();
-            name.SetDefault("Ki Beacon");
+            name.SetDefault("Ki Beacon");            
             AddMapEntry(new Color(223, 245, 255), name);
             dustType = mod.DustType("MetalDust");
+            animationFrameHeight = 18;
             disableSmartCursor = true;
             TileObjectData.addTile(Type);
+        }
+
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            frameCounter++;
+            if (frameCounter > 10)
+            {
+                frame++;
+                frameCounter = 0;
+            }
+            if (frame > 3)
+            {
+                frame = 0;
+            }
         }
 
         public override void NumDust(int i, int j, bool fail, ref int num)
@@ -42,5 +59,44 @@ namespace DBZMOD.Tiles
         {
             Item.NewItem(i * 16, j * 16, 32, 16, mod.ItemType("KiBeaconItem"));
         }
+
+        public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            Tile tile = Main.tile[i, j];
+            Texture2D texture;
+            if (Main.canDrawColorTile(i, j))
+            {
+                texture = Main.tileAltTexture[Type, (int)tile.color()];
+            }
+            else
+            {
+                texture = Main.tileTexture[Type];
+            }
+            Vector2 zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
+            if (Main.drawToScreen)
+            {
+                zero = Vector2.Zero;
+            }
+            int height = 16;
+            int animate = Main.tileFrame[Type] * animationFrameHeight;
+            
+            Main.spriteBatch.Draw(texture, new Vector2(i * 16 - (int)Main.screenPosition.X, j * 16 - (int)Main.screenPosition.Y) + zero, new Rectangle(tile.frameX, tile.frameY + animate, 16, height), Lighting.GetColor(i, j), 0f, default(Vector2), 1f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(mod.GetTexture("Tiles/KiBeaconTileGlowmask"), new Vector2(i * 16 - (int)Main.screenPosition.X, j * 16 - (int)Main.screenPosition.Y) + zero, new Rectangle(tile.frameX, tile.frameY + animate, 16, height), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+
+            DebugUtil.Log(string.Format("FrameX {0}", tile.frameX));
+            if (tile.frameX == 36)
+            {
+                
+                Texture2D lightTexture = mod.GetTexture("Tiles/KiBeaconTileLight2");
+                Vector2 spritePosition = new Vector2(i * 16f - Main.screenPosition.X - 8, (j * 16f - Main.screenPosition.Y) - 14) + zero;
+                spriteBatch.End();
+                spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive);
+                spriteBatch.Draw(lightTexture, spritePosition, new Rectangle(0, 0, lightTexture.Width, lightTexture.Height), Color.White, 0f, lightTexture.Size() * 0.5f, 1f, SpriteEffects.None, 0f);
+                spriteBatch.End();
+                spriteBatch.Begin();
+
+            }
+            return false;
+        }        
     }
 }
