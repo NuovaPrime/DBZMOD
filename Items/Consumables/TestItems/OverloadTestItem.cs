0913afd2c8b753f9f243a4e0b1dcbcf65ea7fdf2
NuovaPrime <loganman51@hotmail.com>
2019-01-29 20:02:41 -0700

    Overload test items

---
+++ Items/Consumables/TestItems/OverloadTestItem.cs
@@ -0,0 +1,40 @@
+﻿using DBZMOD.Enums;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DBZMOD.Items.Consumables.TestItems
+{
+    public class OverloadTestItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 40;
+            item.height = 40;
+            item.consumable = false;
+            item.maxStack = 1;
+            item.UseSound = SoundID.Item3;
+            item.useStyle = 2;
+            item.useTurn = true;
+            item.useAnimation = 17;
+            item.useTime = 17;
+            item.value = 0;
+            item.expert = true;
+            item.potion = false;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Overload Test Item");
+            Tooltip.SetDefault("Maxes out your overload.");
+        }
+
+
+        public override bool UseItem(Player player)
+        {
+            MyPlayer.ModPlayer(player).overloadCurrent = MyPlayer.ModPlayer(player).overloadMax;
+            return true;
+
+        }
+    }
+}
