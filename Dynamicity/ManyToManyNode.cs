a2c0bbcca430d58d9e15b92febb1fdc559f7b1e7
webmilio <therealwebmilio@gmail.com>
2019-02-16 18:53:43 -0500

    Created ManyToMany node to use when building trees (for example Transformations and Skills).

---
+++ Dynamicity/ManyToManyNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DBZMOD.Dynamicity
+{
+    public class ManyToManyNode<T> where T : class
+    {
+        public ManyToManyNode(T[] previous, T current, T[] next)
+        {
+            Previous = previous;
+            Current = current;
+            Next = next;
+        }
+
+        public T[] Previous { get; }
+
+        public T Current { get; }
+
+        public T[] Next { get; }
+    }
+}
