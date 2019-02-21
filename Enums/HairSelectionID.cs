817556719ae934bb641e3aabd240901fa148802a
NuovaPrime <loganman51@hotmail.com>
2019-02-20 20:04:33 -0700

    Needs to be merged.

--- Enums/HairSelectionID.cs
+++ Enums/HairSelectionID.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DBZMOD.Enums.Hair_Enums
+namespace DBZMOD.Enums
 {
     public enum BaseHairSelectionID : int
     {
