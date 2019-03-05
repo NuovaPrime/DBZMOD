a4f73c6dbd586677ecf7c53a1760cb718d5275c0
Blockaroz <lbpk.123fan@gmail.com>
2019-03-04 19:02:56 -0500

    someone please add the hair color thing to this

---
+++ Items/Armor/Vanity/FamiliarBrow.cs
@@ -0,0 +1,32 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace DBZMOD.Items.Armor.Vanity
+{
+    [AutoloadEquip(EquipType.Face)]
+    public class FamiliarBrow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Familiar Brow");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 12;
+            item.value = 3000;
+            item.vanity = true;
+			item.accessory = true;
+        }
+		
+		/*public override Color? GetAlpha(Color lightColor)
+        {
+			return ;
+        }*/
+    }
+}
